1f3473be4f1b30848e8b9ebc800fbe90ea3543b0
Arslan Nasir <arslan@mobylogix.com>
2017-11-13 18:28:18 +0500

    Update TagBox.jsx

--- src/TagBox.jsx
+++ src/TagBox.jsx
@@ -1,12 +1,7 @@
-import { PropTypes } from 'react'
 import TagProp from './utils'
 import TagContainer from './TagBoxContainer'
 
 export default class TagBox extends TagContainer {
-  static propTypes = {
-    tags: PropTypes.arrayOf(TagProp)
-  }
-
   tags() {
     return this.props.tags
   }
